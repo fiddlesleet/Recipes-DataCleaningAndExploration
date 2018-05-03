1131c0d976d53f89cd625a839d4c20e26c039070
fiddlesleet <chris.smythe30@gmail.com>
2018-05-03 12:09:17 -0700

    Update setup_python_db-api.py

--- setup_python_db-api.py
+++ setup_python_db-api.py
@@ -3,14 +3,14 @@ import sqlite3 # library for the database used
 '''
 Return a connection object
 '''
-connection = sqlite3.connect("Cookies") # Cookies is the name of the DB we're connecting to
+db_connection = sqlite3.connect("Cookies") # Cookies is the name of the DB we're connecting to
 
 '''
 Return cursor object, which runs queries & fetches results
     It's called a cursor because it's when the DB returns results, you use the cursor
     to scan through results, just like the cursor in a text editor
 '''
-cursor = connection.cursor()
+cursor = db_connection.cursor()
 
 # execute query using cursor
 query = "select host_key from cookies limit 10"
@@ -22,4 +22,4 @@ print results
 
 #connection.commit() an insertion here, or connection.rcollback() if something went wrong
 
-connection.close()
+db_connection.close()
